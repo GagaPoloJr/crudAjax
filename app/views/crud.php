4b7253018796b1fbd02a2feec4acbb3b215a55a4
Sai Tutor <58431413+SaiTutor@users.noreply.github.com>
2019-12-11 16:56:04 +0700

    Initial commit

---
+++ app/views/crud.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CRUD AJAX</title>
+	<link rel="stylesheet" href="<?php echo base_url().'assets/bootstrap/css/bootstrap.min.css';?>">
+	<link rel="stylesheet" href="<?php echo base_url().'assets/dataTable/datatables.min.css';?>">
+	<link rel="stylesheet" href="<?php echo base_url().'assets/swal/sweetalert.css';?>">
+	<link rel="stylesheet" href="<?php echo base_url().'assets/font-awesome/css/font-awesome.min.css';?>">
+	<script>var BASE_URL = "<?php echo base_url(); ?>";</script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 p-5">
+				<div class="card">
+					<div class="card-header bg-danger text-center text-white">
+						<h3>CRUD AJAX</h3>
+					</div>
+					<div class="card-body">
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script src="<?php echo base_url().'assets/js/jquery-3.4.1.js';?>"></script>
+	<script src="<?php echo base_url().'assets/bootstrap/js/bootstrap.min.js';?>"></script>
+	<script src="<?php echo base_url().'assets/dataTable/datatables.min.js';?>"></script>
+	<script src="<?php echo base_url().'assets/swal/sweetalert.min.js';?>"></script>
+	<script src="<?php echo base_url().'assets/js/ajax/custom.msg.js';?>"></script>
+	<script src="<?php echo base_url().'assets/js/ajax/style.js';?>"></script>
+</body>
+</html>
